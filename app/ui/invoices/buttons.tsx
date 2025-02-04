1679ff28a6553560b15de7346b88a5b7f0fad83c
Manimaran S <maran.sum@gmail.com>
2025-02-04 10:14:48 +0000

    readonly props

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -14,7 +14,7 @@ export function CreateInvoice() {
   );
 }
 
-export function UpdateInvoice({ id }: { id: string }) {
+export function UpdateInvoice({ id }: Readonly<{ id: string }>) {
   return (
     <Link
       href={`/dashboard/invoices/${id}/edit`}
@@ -25,7 +25,7 @@ export function UpdateInvoice({ id }: { id: string }) {
   );
 }
 
-export function DeleteInvoice({ id }: { id: string }) {
+export function DeleteInvoice({ id }: Readonly<{ id: string }>) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id)
   return (
     <form action={deleteInvoiceWithId}>
